ea8a442b623250dc7a29a9c457afad81a1a4fa34
chane81 <chane81@naver.com>
2023-08-13 03:23:02 +0900

    feat: textfield editing submit

--- lib/features/authentication/email_screen.dart
+++ lib/features/authentication/email_screen.dart
@@ -89,7 +89,7 @@ class _EmailScreenState extends State<EmailScreen> {
               TextField(
                 controller: _emailController,
                 keyboardType: TextInputType.emailAddress,
-                // onEditingComplete: _onSubmit,
+                onEditingComplete: _onSubmit,
                 autocorrect: false,
                 decoration: InputDecoration(
                   hintText: 'Email',
