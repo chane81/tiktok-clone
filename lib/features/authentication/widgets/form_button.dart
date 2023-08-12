0f74c3d1904e17d8d47b50646539f250770dd1cb
chane81 <chane81@naver.com>
2023-08-13 03:18:42 +0900

    feat: email screen

--- lib/features/authentication/widgets/form_button.dart
+++ lib/features/authentication/widgets/form_button.dart
@@ -5,33 +5,39 @@ class FormButton extends StatelessWidget {
   const FormButton({
     super.key,
     required this.disabled,
+    this.onTab,
   });
 
   final bool disabled;
+  final void Function()? onTab;
 
   @override
   Widget build(BuildContext context) {
-    return FractionallySizedBox(
-      widthFactor: 1,
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 300),
-        padding: const EdgeInsets.symmetric(
-          vertical: Sizes.size16,
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(Sizes.size5),
-          color:
-              disabled ? Colors.grey.shade300 : Theme.of(context).primaryColor,
-        ),
-        child: AnimatedDefaultTextStyle(
+    return GestureDetector(
+      onTap: onTab,
+      child: FractionallySizedBox(
+        widthFactor: 1,
+        child: AnimatedContainer(
           duration: const Duration(milliseconds: 300),
-          style: TextStyle(
-            color: disabled ? Colors.grey.shade400 : Colors.white,
-            fontWeight: FontWeight.w600,
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size16,
           ),
-          child: const Text(
-            'Next',
-            textAlign: TextAlign.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(Sizes.size5),
+            color: disabled
+                ? Colors.grey.shade300
+                : Theme.of(context).primaryColor,
+          ),
+          child: AnimatedDefaultTextStyle(
+            duration: const Duration(milliseconds: 300),
+            style: TextStyle(
+              color: disabled ? Colors.grey.shade400 : Colors.white,
+              fontWeight: FontWeight.w600,
+            ),
+            child: const Text(
+              'Next',
+              textAlign: TextAlign.center,
+            ),
           ),
         ),
       ),
